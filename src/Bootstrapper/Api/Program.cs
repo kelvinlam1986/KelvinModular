e3c59fa3ae8effa8d13320f7fdff3bfe08b9cec0
kelvinlam1986 <kelvincoder@gmail.com>
2025-10-31 11:36:48 +0700

    Add and register basket module and ordering module

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,10 +1,14 @@
 using Catalog;
+using Basket;
+using Ordering;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services
-    .AddCatalogModule(builder.Configuration);
+    .AddCatalogModule(builder.Configuration)
+    .AddBaksetModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Basket
+{
+    public static class BasketModule
+    {
+        public static IServiceCollection AddBaksetModule(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
