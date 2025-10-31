01ad94bfb27c70a14390e144de7ebaf8a6d35f03
kelvinlam1986 <kelvincoder@gmail.com>
2025-10-31 11:32:39 +0700

    Add and register DI Catalog module

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,6 +1,10 @@
+using Catalog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services
+    .AddCatalogModule(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Catalog
+{
+    public static class CatalogModule
+    {
+        public static IServiceCollection AddCatalogModule(this IServiceCollection service, 
+            IConfiguration configuration)
+        {
+            return service;
+        }
+    }
+}
