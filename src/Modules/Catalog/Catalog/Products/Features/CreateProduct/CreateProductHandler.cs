3209f0e1c8f1f364f764644dca15fe4778f65f8c
Minh Lam <kelvincoder@gmail.com>
2025-11-02 02:13:40 -0800

    Create abstraction for CQRS

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Bussiness create product logic 
+            throw new NotImplementedException();
+        }
+    }
+}
