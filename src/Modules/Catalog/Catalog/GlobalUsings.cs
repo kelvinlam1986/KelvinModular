5885816bc38eeda7e846687454f6233512d7be45
kelvinlam1986 <kelvincoder@gmail.com>
2025-11-03 11:49:35 +0700

    Implement endpoint create, update, delete product

--- src/Modules/Catalog/Catalog/GlobalUsings.cs
+++ src/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -1,9 +1,13 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Logging;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
 global using System.Reflection;
 global using Mapster;
 global using MediatR;
+global using Carter;
 
 global using Shared.DDD;
 global using Shared.CQRS;
@@ -11,6 +15,8 @@ global using Catalog.Products.Models;
 global using Catalog.Products.Events;
 global using Catalog.Data;
 global using Catalog.Products.Dtos;
+
+
 
 
 
