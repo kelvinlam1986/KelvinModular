8c47e40fee2c68a4f1fa52413d1c8ce6c75292a5
kelvinlam1986 <kelvincoder@gmail.com>
2025-11-03 11:11:18 +0700

    Implement domain event handler for Product Catalog

--- src/Modules/Catalog/Catalog/GlobalUsings.cs
+++ src/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -1,7 +1,9 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.Logging;
 global using System.Reflection;
 global using Mapster;
+global using MediatR;
 
 global using Shared.DDD;
 global using Shared.CQRS;
