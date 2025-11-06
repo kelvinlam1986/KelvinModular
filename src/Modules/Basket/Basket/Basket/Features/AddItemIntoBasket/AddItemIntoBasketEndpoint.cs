b5ce0918f22c50a7791c6f3b9b3690205748eb79
kelvinlam1986 <kelvincoder@gmail.com>
2025-11-06 10:18:47 +0700

    implement add item into basket endpoint

---
+++ src/Modules/Basket/Basket/Basket/Features/AddItemIntoBasket/AddItemIntoBasketEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace Basket.Basket.Features.AddItemIntoBasket
+{
+    public record AddItemIntoBasketRequest(string UserName, ShoppingCartItemDto ShoppingCartItem);
+
+    public record AddItemIntoBasketResponse(Guid Id);
+
+    public class AddItemIntoBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket/{userName}/items",
+                async ([FromRoute] string userName,
+                       [FromBody] AddItemIntoBasketRequest request,
+                       ISender sender) =>
+                {
+                    var command = new AddItemIntoBasketCommand(userName, request.ShoppingCartItem);
+                    var result = await sender.Send(command);
+                    var response = result.Adapt<AddItemIntoBasketResponse>();
+                    return Results.Created($"/basket/{response.Id}", response);
+                })
+                .WithName("AddItemToBaseKet")
+                .Produces<AddItemIntoBasketResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithDescription("Add Item to Basket")
+                .WithSummary("Add Item to Basket");
+        }
+    }
+}
--- src/Modules/Basket/Basket/GlobalUsings.cs
+++ src/Modules/Basket/Basket/GlobalUsings.cs
@@ -7,6 +7,7 @@ global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.AspNetCore.Routing;
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
 global using FluentValidation;
 global using Mapster;
 global using Carter;
